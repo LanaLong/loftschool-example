618b47d1209ef77d57d327626f183bc8516262d2
lanalong <qmexperience@gmail.com>
2019-03-03 12:36:42 +0300

    isMatching & fix typos

--- src/towns.js
+++ src/towns.js
@@ -12,17 +12,17 @@
 
  Разметку смотрите в файле towns-content.hbs
 
- Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер.
 
  *** Часть со звездочкой ***
  Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
  то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
- При клике на кнопку, процесс загруки повторяется заново
+ При клике на кнопку, процесс загрузки повторяется заново.
  */
 
 /*
- homeworkContainer - это контейнер для всех ваших домашних заданий
- Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+ homeworkContainer - это контейнер для всех ваших домашних заданий.
+ Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер.
 
  Пример:
    const newDiv = document.createElement('div');
@@ -31,17 +31,27 @@
 const homeworkContainer = document.querySelector('#homework-container');
 
 /*
- Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+ Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения.
 
- Массив городов пожно получить отправив асинхронный запрос по адресу
+ Массив городов можно получить, отправив асинхронный запрос по адресу:
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
  */
 function loadTowns() {
+  var url = ('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+
+  return fetch(url)
+    .then(response => {
+      if (response.status >= 400) {
+          return Promise.reject();
+      }
+      return response.json();
+    })
+    .then(cities => cities.sort((a, b) => a.name.localeCompare(b.name)));
 }
 
 /*
- Функция должна проверять встречается ли подстрока chunk в строке full
- Проверка должна происходить без учета регистра символов
+ Функция должна проверять встречается ли подстрока chunk в строке full.
+ Проверка должна происходить без учета регистра символов.
 
  Пример:
    isMatching('Moscow', 'moscow') // true
@@ -51,6 +61,8 @@ function loadTowns() {
    isMatching('Moscow', 'Moscov') // false
  */
 function isMatching(full, chunk) {
+  let searchResponse = (full.toLowerCase().indexOf(chunk.toLowerCase()) != -1) ? true : false;
+  return searchResponse;
 }
 
 /* Блок с надписью "Загрузка" */
@@ -63,7 +75,7 @@ const filterInput = homeworkContainer.querySelector('#filter-input');
 const filterResult = homeworkContainer.querySelector('#filter-result');
 
 filterInput.addEventListener('keyup', function() {
-    // это обработчик нажатия кливиш в текстовом поле
+    // это обработчик нажатия клавиш в текстовом поле
 });
 
 export {
