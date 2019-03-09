4b2c4412d3a9990f453924ddcaae2757d6023dfc
lanalong <qmexperience@gmail.com>
2019-03-09 22:18:02 +0300

    webpack-4-zero initial commit __ for start project

---
+++ src/index.js
@@ -0,0 +1,54 @@
+console.log(111);
+
+//import { initMap } from './js/ymaps';
+
+// window.onload = initMap();
+
+import render from './templates/friends.hbs';
+
+const map = document.querySelector('#map');
+
+const list = [
+  {
+    name: 'Иван',
+    last_name: 'Иванов',
+  },
+  {
+    name: 'Олег',
+    last_name: 'Иванов',
+  },
+  {
+    name: 'Денис',
+    last_name: 'Иванов',
+  },
+  {
+    name: 'Иван',
+    last_name: 'Иванов',
+  },
+  {
+    name: 'Олег',
+    last_name: 'Иванов',
+  },
+  {
+    name: 'Денис',
+    last_name: 'Иванов',
+  },
+  {
+    name: 'Иван',
+    last_name: 'Иванов',
+  },
+  {
+    name: 'Олег',
+    last_name: 'Иванов',
+  },
+  {
+    name: 'Денис',
+    last_name: 'Иванов',
+  },
+  {
+    name: 'Оля',
+    last_name: 'Иванов',
+  }
+];
+
+map.innerHTML = render({list});
