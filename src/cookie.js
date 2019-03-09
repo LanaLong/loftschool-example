8e5ef59f4937200fc71cefae0bc0474c7e555a6e
lanalong <qmexperience@gmail.com>
2019-03-09 17:06:34 +0300

    fifty-fifty

--- src/cookie.js
+++ src/cookie.js
@@ -16,9 +16,9 @@
  7.3: На странице должно быть текстовое поле для фильтрации cookie.
  В таблице должны быть только те cookie, в имени или значении которых, хотя бы частично, есть введенное значение.
  Если в поле фильтра пусто, то должны выводиться все доступные cookie.
- Если дабавляемая cookie не соответствует фильтру, то она должна быть добавлена только в браузер, но не в таблицу.
+ Если добавляемая cookie не соответствует фильтру, то она должна быть добавлена только в браузер, но не в таблицу.
  Если добавляется cookie, с именем уже существующией cookie и ее новое значение не соответствует фильтру,
- то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
+ то ее значение должно быть обновлено в браузере, и из таблицы cookie должна быть удалена
 
  Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер.
  */
@@ -43,10 +43,59 @@ const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
-filterNameInput.addEventListener('keyup', function() {
-    // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
+function getCookies() {
+  return document.cookie
+    .split('; ')
+    .filter(Boolean)
+    .map(cookie => cookie.match(/^([^=]+)=(.+)/))
+    .reduce((obj, [, name, value]) => {
+      obj[name] = value;
+
+      return obj;
+    }, {});
+}
+
+// matching function
+function isMatch(full, chunk) {
+  if (full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+renderTable();
+
+//delete cookie
+listTable.addEventListener('click', function (e) {
+  // console.log(e.target);
+  if (e.target.dataset.key) {
+    document.cookie = e.target.dataset.key + `=;expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+  }
+  renderTable();
 });
 
-addButton.addEventListener('click', () => {
-    // здесь можно обработать нажатие на кнопку "добавить cookie"
+// handle search key
+filterNameInput.addEventListener('keyup', function () {
+  renderTable();
 });
+
+// add cookie
+addButton.addEventListener('click', () => {
+  document.cookie = `${addNameInput.value} = ${addValueInput.value}`;
+  renderTable();
+
+  addNameInput.value = '';
+  addValueInput.value = '';
+});
+
+// render
+function renderTable() {
+  const cookie_obj = getCookies();
+  listTable.innerHTML = '';
+  for (let key in cookie_obj) {
+    if (!(isMatch(key, filterNameInput.value) || isMatch(cookie_obj[key], filterNameInput.value))) continue;
+    listTable.innerHTML += `<tr><td class="first_td">${key}</td><td>${cookie_obj[key]}</td><td><button class="del" 
+        data-key="${key}">Удалить</button></td></tr>`;
+  }
+}
